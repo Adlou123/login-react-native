bb045e9cc06ffa35773331005bbf2518dc5180f6
Adlou123 <139489793+Adlou123@users.noreply.github.com>
2025-09-16 16:15:10 -0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput, Button, Image } from 'react-native';
+import styles from './style'
+
+
+export default function App() {
+  const [ usuario, setUsuario ] = useState('');
+  const [ senha, setSenha ] = useState('');
+  const [ logado, setLogado ] = useState(false);
+
+  function fazerLogin(){
+    if (usuario === 'Adm' && senha === '132'){
+      setLogado(true)
+    } else {
+      alert('Usuário ou senha incorretos!!')
+    }
+  }
+
+  if(!logado) {  
+  return (
+    <View style={styles.container}>
+      <Image style={styles.logo} source={{uri: 'https://www.svgrepo.com/show/333590/react.svg'}} />
+      <TextInput style={styles.input}
+       placeholder='Usuário'
+       value={usuario}
+       onChangeText={setUsuario}
+    />
+      <TextInput style={styles.input}
+        placeholder='Senha'
+        value={senha}
+        onChangeText={setSenha}
+        secureTextEntry
+    />
+      <Button title='Fazer Login' onPress={fazerLogin} />
+    </View>
+  );
+  }
+
+  return(
+    <View style={styles.container}>
+    <Text style={styles.bemVindo}>Sistema logado com sucessoo!!</Text>
+    <Text style={styles.nomeTopo}>Seja bem-vindo, { usuario }</Text>
+    </View>
+  );
+
+}
